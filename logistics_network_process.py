f1241955fddd765101e0b20d28df774d1d02e370
Johnwangbin <1103628739@qq.com>
2016-11-01 13:51:50 +0800

    complete the logistics network  back_end code

--- logistics_network_process.py
+++ logistics_network_process.py
@@ -1,5 +1,4 @@
 #coding:utf-8
-import time
 from possion_manager import NegativeExponentialDist, depend_direction
 from two_list_contruct import two_list_merge
 
@@ -8,19 +7,25 @@ reach_time = 2
 
 
 class LogisticsHouse(object):
-    def __init__(self, left_house, right_house, rate):
+    def __init__(self, p_lambda, p_miu, n, worker_num):
         self.queue_system = None
         self.to_left_house = []
         self.to_right_house = []
-        self.to_local_truck_left = left_house.to_right_house
-        self.to_local_truck_right = right_house.to_left_house
+        self.to_local_truck_left = None
+        self.to_local_truck_right = None
         self.to_local_stream = []
-        self.rate = rate
-
-    def initial_queue_system(self, p_lambda, p_miu, n, worker_num):
+        self.rate = 0.0
         self.queue_system = QueueSystem(p_lambda, p_miu, n, worker_num)
 
+    def initial_relative_houses(self, rate, left_house, right_house):
+        self.rate = rate
+        self.to_local_truck_left = left_house.to_right_house
+        self.to_local_truck_right = right_house.to_left_house
+
     def generate_two_output_streams(self):
+        self.queue_system.generate_service_completed_time()
+        self.queue_system.generate_output_stream()
+        self.queue_system.generate_waited_length_stream()
         for point in self.queue_system.output_stream:
             if depend_direction(self.rate):
                 self.to_left_house.append(point)
@@ -36,7 +41,7 @@ class LogisticsHouse(object):
 class QueueSystem(object):
     def __init__(self, p_lambda, p_miu, n, worker_num):
         self.servers_intervals = []
-        self.service_streams_current_index = [0] * 3
+        self.service_streams_current_index = [0] * worker_num
         self.truck_stream       = []
         self.service_streams    = []
         self.waited_truck       = []
@@ -63,13 +68,13 @@ class QueueSystem(object):
 
         self.service_streams = []
         for i in range(self.worker_num):
-            self.service_streams.append(list(self.service_dist.produce_mass_random_num(self.truck_amount / 3)))
+            self.service_streams.append(list(self.service_dist.produce_mass_random_num(self.truck_amount)))
 
     def server_first_finished(self):
         finished_time = []
         for server_intervals in self.servers_intervals:
             finished_time.append(server_intervals[-1][1])
-        min_time = 0.0
+        min_time = finished_time[0]
         min_index = 0
         for i, point in enumerate(finished_time):
             if point < min_time:
@@ -78,7 +83,6 @@ class QueueSystem(object):
         return min_index, min_time
 
     def add_truck_service_interval(self, server_num, start_time):
-        print self.service_streams_current_index[server_num]
         self.servers_intervals[server_num].append((start_time, start_time + self.service_streams[server_num][
             self.service_streams_current_index[server_num]]))
         self.service_streams_current_index[server_num] += 1
@@ -93,11 +97,11 @@ class QueueSystem(object):
 
     def generate_output_stream(self):
         for server_intervals in self.servers_intervals:
-            self.output_stream.extend([server_interval[1] for server_interval in server_intervals])
+            self.output_stream.extend([server_interval[1] for server_interval in server_intervals[1:]])
         self.output_stream.sort()
 
     def generate_waited_length_stream(self):
-        stream = two_list_merge("input", "output", self.truck_stream, self.output_stream)
+        stream = two_list_merge("input", "output", self.truck_reach_time, self.output_stream)
         for tag, point in stream:
             if tag == "input":
                 self.waited_num += 1
@@ -109,9 +113,25 @@ class QueueSystem(object):
 def test_queue_system():
     queue_system = QueueSystem(2, 2, 9999, 3)
     queue_system.generate_service_completed_time()
-    print queue_system.servers_intervals
-    # queue_system.generate_output_stream()
-    # queue_system.generate_waited_length_stream()
+    queue_system.generate_output_stream()
+    queue_system.generate_waited_length_stream()
+    print queue_system.waited_num_stream
+
+
+def test_logistics_house():
+    house_a = LogisticsHouse(2, 2, 9999, 3)
+    house_b = LogisticsHouse(2, 2, 9999, 4)
+    house_c = LogisticsHouse(2, 2, 9999, 2)
+    house_a.initial_relative_houses(0.6, house_b, house_c)
+    house_b.initial_relative_houses(0.3, house_c, house_a)
+    house_c.initial_relative_houses(0.4, house_a, house_b)
+
+    house_a.generate_two_output_streams()
+    house_b.generate_two_output_streams()
+    house_c.generate_two_output_streams()
+
+    house_a.generate_to_local_truck_stream()
 
 if __name__ == "__main__":
-    test_queue_system()
+    # test_queue_system()
+    test_logistics_house()
--- two_list_contruct.py
+++ two_list_contruct.py
@@ -6,5 +6,5 @@ def two_list_merge(tag1, tag2, stream1, stream2):
     stream.extend([(tag1, element) for element in stream1])
     stream.extend([(tag2, element) for element in stream2])
 
-    stream.sort(key=itemgetter(2))
+    stream.sort(key=itemgetter(1))
     return stream
